d0d5164a89e90d7b95079af603b41776b8d072c2
Yoav Bergner <ybergner@gmail.com>
2019-06-04 19:55:18 -0400

    money and color-blindness

---
+++ cbColors.R
@@ -0,0 +1,22 @@
+## Transparent colors
+## Mark Gardener 2015
+## www.dataanalytics.org.uk
+t_col <- function(color, percent = 50, name = NULL) {
+  #	  color = color name
+  #	percent = % transparency
+  #	   name = an optional name for the color
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100-percent)*255/100,
+               names = name)
+  ## Save the color
+  invisible(t.col)
+
+}
+## END
+##
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
